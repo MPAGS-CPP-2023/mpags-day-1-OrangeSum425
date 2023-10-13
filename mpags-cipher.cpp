bcb62186a2d47511dfdf58f9a3aab324e3458777
Yaoqi Cao <99253867+OrangeSum425@users.noreply.github.com>
2023-10-13 12:30:44 +0000

    add ex3 for cpp

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <string>
 int main()
-{
+{  
+   //ex1
    std::cout<<"Hello World!\n";
-
-   int a = 5;
-   double b =6.1;
-   c =a*b;
+   //ex3_string
+   std::string mgs {"Hello"};
+   std::cout<<mgs<<"\n";
+   //ex2
+   int a{5};
+   double b {6.1};
+   std::cout<<b<<std::endl;  
+   double c =a*b;
    std::cout << c << std::endl;
-
-   
+   const double test{4.5};
+   std::cout<<test<<std::endl;
+   int d{5};
+   d=5.4;
+   std::cout<<d<<std::endl;   
 }
