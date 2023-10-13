1853f5475fedaa5db69020b8594354dcf56e5041
Yaoqi Cao <99253867+OrangeSum425@users.noreply.github.com>
2023-10-13 12:18:19 +0000

    add ex2 for cpp

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 int main()
 {
-std::cout<<"Hello World!\n";
+   std::cout<<"Hello World!\n";
+
+   int a = 5;
+   int b =6.1;
+   c =a*b;
+   std::cout << c << std::endl;
+
+   
 }
